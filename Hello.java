d8e0c9834afb53562ffcca1a6a3cae06f4d75b71
Hari <harivardhanreddy8955@gmail.com>
2026-03-16 08:24:49 -0700

    This is the second commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello{
     public static void main (String[] args){
         System.out.println("Hello");
+        int a = 3;
+        double b = 3.3 ;
+        System.out.println("The value of a + b is " + (a + b));
     }
 }
