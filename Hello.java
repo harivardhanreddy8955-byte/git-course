22806b8a0aa865398055058e02c16ea96bd0d50f
Hari <harivardhanreddy8955@gmail.com>
2026-03-16 09:40:22 -0700

    user added

--- Hello.java
+++ Hello.java
@@ -3,6 +3,9 @@ public class Hello{
         System.out.println("Hello");
         int a = 3;
         double b = 3.3 ;
+       // System.out.println("The value of a + b is " + (a + b));
+        a = 5 ;
         System.out.println("The value of a + b is " + (a + b));
+        a = 6 ;
     }
 }
