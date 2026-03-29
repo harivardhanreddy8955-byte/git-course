1dd91023b71f9b0154c3880c545c4aa9b0723659
Hari <harivardhanreddy8955@gmail.com>
2026-03-29 09:30:18 -0700

    Sql commit

---
+++ Sql.java
@@ -0,0 +1,8 @@
+// Structured query langauge 
+ select * from table_name ; // gives all the data from the table.
+ select name, id , salaary from table_name ;// gives what we want 
+
+
+ SELECT column_name
+FROM table_name
+WHERE condition;
